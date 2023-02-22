ac1453b4707f2bd5311a767bc35d02c24f0da6bc
Abraham <brhmdeen@gmail.com>
2023-02-22 17:46:36 +0100

    first commit

---
+++ stackPalChecker.py
@@ -0,0 +1,46 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class Stack:
+    def __init__(self):
+        self.top = None
+
+    def push(self, value):
+        new_node = Node(value)
+        new_node.next = self.top
+        self.top = new_node
+
+    def pop(self):
+        if self.top is None:
+            return None
+        else:
+            popped_node = self.top
+            self.top = self.top.next
+            return popped_node.value
+    
+    def is_empty(self):
+        return self.top is None
+
+
+def is_palindrome(word):
+    stack = Stack() 
+    for letter in word:
+        stack.push(letter)
+    reverse_word = ''
+    while not stack.is_empty():
+        reverse_word += stack.pop()
+    if word.lower() == reverse_word.lower():
+        return True
+    else:
+        return False
+
+
+with open('palindrome.txt', 'r') as file:
+    for line in file:
+        word = line.strip()
+        if is_palindrome(word):
+            print(word, 'is a proper palindrome')
+        else:
+            print(word, 'is NOT a proper palindrome')
